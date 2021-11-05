5f9d83be50500d157373685085dc4e7fb8665418
IreliaBot <andreysil021@gmail.com>
2021-11-06 00:52:47 +0500

    F.5.6. Знакомство с маршртизацией в React

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@
   import Header from'./components/header/Header.js'
   import Navbar from './components/navbar/Navbar.js'
   import Profile from './components/profile/Profile.js'
+  import Dialogs from './components/dialogs/Dialogs.js'
+  import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 
   function App() {
     return (
+      
         <div className="wrapper">
-          <Header/>
-          <Navbar/>
-          <Profile/>
-        </div>
+          <BrowserRouter> 
+            <Header/>
+            <Navbar/>
+            <Switch>
+              <Route exact path='/' component={Profile} />
+              <Route exact path='/profile' component={Profile} />
+              <Route exact path='/dialogs' component={Dialogs} />
+            </Switch>
+          </BrowserRouter> 
+        </div>  
     )
   }
 export default App
